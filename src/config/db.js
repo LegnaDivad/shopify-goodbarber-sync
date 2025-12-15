114e537de9cf70e99443ceaa490a42aea1dd5f1a
Angel Jonguitud <angel.pena5426@alumnos.udg.mx>
2025-12-14 20:52:43 -0600

    git commit -m "fix(db): force IPv4 DNS lookup to avoid IPv6 ENETUNREACH"

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,15 @@
 const { Pool } = require('pg');
+const dns = require('dns');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // Supabase/Render suele requerir SSL
+  ssl: { rejectUnauthorized: false },
+
+  // Fuerza resolución IPv4 (evita ENETUNREACH por IPv6)
+  lookup: (hostname, options, callback) => {
+    dns.lookup(hostname, { family: 4, all: false }, callback);
+  },
+
   max: 10,
   idleTimeoutMillis: 30_000,
   connectionTimeoutMillis: 10_000,
