4e0a04781ba63391de77aa7a3a1dca4afb02aa62
Angel Jonguitud <angel.pena5426@alumnos.udg.mx>
2025-12-17 18:43:08 -0600

    feat(csv): add product summary to rows in GoodBarber CSV export

--- src/sync/buildGoodbarberCsv.js
+++ src/sync/buildGoodbarberCsv.js
@@ -1,6 +1,7 @@
 const { stringify } = require('csv-stringify/sync');
 const slugify = require('../utils/slugify');
 const toGoodbarberOptions = require('../utils/goodbarberOptions');
+const { stripHtml, truncate } = require('../utils/transform');
 
 function buildRowsFromShopify(products) {
   const rows = [];
@@ -8,6 +9,7 @@ function buildRowsFromShopify(products) {
   for (const p of products) {
     const productTitle = p.title || '';
     const productSlug = p.handle ? p.handle : slugify(productTitle);
+    const productSummary = truncate(stripHtml(p.body_html || ''), 240);
 
     const productImage = (p.image && p.image.src) ? p.image.src : '';
 
@@ -25,7 +27,7 @@ function buildRowsFromShopify(products) {
         product_id: '',                 // vacío para altas nuevas en GoodBarber
         variant_id: '',                 // vacío para altas nuevas en GoodBarber
         product_title: productTitle,
-        product_summary: '',
+        product_summary: productSummary,
         product_url_slug: productSlug,
         variant_options: toGoodbarberOptions(p, v), // [[size:36]][[color:red]]
         variant_stock: stock,
